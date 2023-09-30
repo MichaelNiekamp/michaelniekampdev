3300a9c1f8a3dd4867e776aaf9e2fbf7b71b1cf3
Michael Niekamp <michael.niekamp@umconnect.umt.edu>
2023-09-30 17:37:57 -0600

    initial commit

---
+++ js/popup-slider.js
@@ -0,0 +1,116 @@
+$( document ).ready(function() {
+    $('.btn-slider').on('click', function() {
+      $('.sliderPop').show();
+      $('.ct-sliderPop-container').addClass('open');
+      $('.sliderPop').addClass('flexslider');
+      $('.sliderPop .ct-sliderPop-container').addClass('slides');
+  
+      $('.sliderPop').flexslider({
+      selector: '.ct-sliderPop-container > .ct-sliderPop',
+      slideshow: false,
+      controlNav: false,
+      controlsContainer: '.ct-sliderPop-container'
+      });
+    });
+  
+    $('.ct-sliderPop-close').on('click', function() {
+      $('.sliderPop').hide();
+      $('.ct-sliderPop-container').removeClass('open');
+      $('.sliderPop').removeClass('flexslider');
+      $('.sliderPop .ct-sliderPop-container').removeClass('slides');
+    });
+  });
+
+
+  
+  $( document ).ready(function() {
+    $('.btn-slider2').on('click', function() {
+      $('.sliderPop2').show();
+      $('.ct-sliderPop2-container').addClass('open');
+      $('.sliderPop2').addClass('flexslider');
+      $('.sliderPop2 .ct-sliderPop2-container').addClass('slides');
+  
+      $('.sliderPop2').flexslider({
+      selector: '.ct-sliderPop2-container > .ct-sliderPop2',
+      slideshow: false,
+      controlNav: false,
+      controlsContainer: '.ct-sliderPop2-container'
+      });
+    });
+  
+    $('.ct-sliderPop2-close').on('click', function() {
+      $('.sliderPop2').hide();
+      $('.ct-sliderPop2-container').removeClass('open');
+      $('.sliderPop2').removeClass('flexslider');
+      $('.sliderPop2 .ct-sliderPop2-container').removeClass('slides');
+    });
+  });
+
+  $( document ).ready(function() {
+    $('.btn-slider3').on('click', function() {
+      $('.sliderPop3').show();
+      $('.ct-sliderPop3-container').addClass('open');
+      $('.sliderPop3').addClass('flexslider');
+      $('.sliderPop3 .ct-sliderPop3-container').addClass('slides');
+  
+      $('.sliderPop3').flexslider({
+      selector: '.ct-sliderPop3-container > .ct-sliderPop3',
+      slideshow: false,
+      controlNav: false,
+      controlsContainer: '.ct-sliderPop3-container'
+      });
+    });
+  
+    $('.ct-sliderPop3-close').on('click', function() {
+      $('.sliderPop3').hide();
+      $('.ct-sliderPop3-container').removeClass('open');
+      $('.sliderPop3').removeClass('flexslider');
+      $('.sliderPop3 .ct-sliderPop3-container').removeClass('slides');
+    });
+  });
+
+  $( document ).ready(function() {
+    $('.btn-slider4').on('click', function() {
+      $('.sliderPop4').show();
+      $('.ct-sliderPop4-container').addClass('open');
+      $('.sliderPop4').addClass('flexslider');
+      $('.sliderPop4 .ct-sliderPop4-container').addClass('slides');
+  
+      $('.sliderPop4').flexslider({
+      selector: '.ct-sliderPop4-container > .ct-sliderPop4',
+      slideshow: false,
+      controlNav: false,
+      controlsContainer: '.ct-sliderPop4-container'
+      });
+    });
+  
+    $('.ct-sliderPop4-close').on('click', function() {
+      $('.sliderPop4').hide();
+      $('.ct-sliderPop4-container').removeClass('open');
+      $('.sliderPop4').removeClass('flexslider');
+      $('.sliderPop4 .ct-sliderPop4-container').removeClass('slides');
+    });
+  });
+
+  $( document ).ready(function() {
+    $('.btn-slider5').on('click', function() {
+      $('.sliderPop5').show();
+      $('.ct-sliderPop5-container').addClass('open');
+      $('.sliderPop5').addClass('flexslider');
+      $('.sliderPop5 .ct-sliderPop5-container').addClass('slides');
+  
+      $('.sliderPop5').flexslider({
+      selector: '.ct-sliderPop5-container > .ct-sliderPop5',
+      slideshow: false,
+      controlNav: false,
+      controlsContainer: '.ct-sliderPop5-container'
+      });
+    });
+  
+    $('.ct-sliderPop5-close').on('click', function() {
+      $('.sliderPop5').hide();
+      $('.ct-sliderPop5-container').removeClass('open');
+      $('.sliderPop5').removeClass('flexslider');
+      $('.sliderPop5 .ct-sliderPop5-container').removeClass('slides');
+    });
+  });
